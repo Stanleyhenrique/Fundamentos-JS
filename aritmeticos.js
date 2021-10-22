752dc95fba7cf729c0a5617181d420489feaf7c9
Stanleyhenrique <alunostanleyhenrique@hotmail.com>
2021-10-22 12:45:06 -0300

    Operadores

---
+++ aritmeticos.js
@@ -0,0 +1,9 @@
+const [a,b,c,d] = [1,2,3,4]
+
+const soma = a + b + c + d // 10
+const subtracao = d - a // 3
+const multiplicacao = a * d // 4
+const divisao = c / a // 3
+const modulo = a % 2 // 1
+
+console.log(soma, subtracao, multiplicacao, divisao, modulo)
---
+++ operadoresRelacionais.js
@@ -0,0 +1,49 @@
+console.log('OPERADORES:')
+/* 
+
+O operador Igual ( == ) 
+Descrição ( verdadeiro caso os operandos sejam iguais. )
+Exemplo: 3 == '3'
+outro exemplo:
+
+*/
+console.log('01A)', '1' == 1)
+console.log('01B)', '1' == 2) //exemplo false
+console.log('/***************************/')
+
+/* 
+
+O operador Estritamente igual ( === )
+Descrição ( Retorna verdadeiro caso os operandos sejam iguais e do mesmo tipo. )
+Exemplo:  3 === 3
+outro exemplo:
+
+*/
+console.log('02A)', '2' === 4)
+console.log('02B)', '2' === 3) //exemplo false
+console.log('/***************************/')
+
+/* 
+
+O operador != ( != )
+Descrição ( Retorna verdadeiro caso os operandos não sejam iguais. )
+Exemplo:  
+outro exemplo:
+
+*/
+console.log('03A)', '3' != 3)
+console.log('03B)', '3' != 4)
+console.log('/***************************/')
+
+/* 
+
+O operador Estritamente não igual ( !== )
+Descrição ( Retorna verdadeiro caso os operandos não sejam iguais e/ou não sejam do mesmo tipo. )
+Exemplo:  
+outro exemplo:
+
+*/
+console.log('04A)', '4' !== 4)
+console.log('04B)', '4'!== 5)
+console.log('/***************************/')
+
